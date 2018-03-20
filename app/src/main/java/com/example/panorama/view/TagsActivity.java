e8d67b4ff25983f47f146b475cffcfa14602c12b
IvanGlez1996 <ivanglez1996@gmail.com>
2018-03-20 00:00:23 +0000

    PhotoSphere Preview implemented

--- app/src/main/java/com/example/panorama/view/TagsActivity.java
+++ app/src/main/java/com/example/panorama/view/TagsActivity.java
@@ -11,11 +11,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.InputType;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.ImageView;
 
 import com.example.panorama.R;
+import com.google.vr.sdk.widgets.pano.VrPanoramaView;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -32,7 +33,8 @@ public class TagsActivity extends AppCompatActivity implements ITagsActivity {
     private RecyclerView.LayoutManager mLayoutManager;
     private ArrayList<String> datos;
     private FloatingActionButton addTagBtn;
-    private ImageView imagePreview;
+//    private ImageView imagePreview;
+    private VrPanoramaView mVrPanoramaView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,7 +52,11 @@ public class TagsActivity extends AppCompatActivity implements ITagsActivity {
             }
         });
 
-        imagePreview = (ImageView) findViewById(R.id.imageView);
+        mVrPanoramaView = (VrPanoramaView) findViewById(R.id.pano_view);
+
+        loadPhotoSphere();
+
+        /*imagePreview = (ImageView) findViewById(R.id.imageView);
         Intent i = getIntent();
         String fileName = i.getStringExtra("fileName");
         File imgFile = new  File(fileName);
@@ -61,7 +67,7 @@ public class TagsActivity extends AppCompatActivity implements ITagsActivity {
 
             imagePreview.setImageBitmap(myBitmap);
 
-        }
+        }*/
 
         // use this setting to improve performance if you know that changes
         // in content do not change the layout size of the RecyclerView
@@ -97,6 +103,45 @@ public class TagsActivity extends AppCompatActivity implements ITagsActivity {
             }
         });
         alert.show();
+    }
+
+    private void loadPhotoSphere() {
+        //This could take a while. Should do on a background thread, but fine for current example
+        VrPanoramaView.Options options = new VrPanoramaView.Options();
+
+
+        Intent i = getIntent();
+        String fileName = i.getStringExtra("fileName");
+        File imgFile = new  File(fileName);
+
+        if(imgFile.exists()) {
+
+            Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+
+
+                options.inputType = VrPanoramaView.Options.TYPE_MONO;
+                mVrPanoramaView.loadImageFromBitmap(myBitmap, options);
+        }else{
+            Log.d("Error", "La imagen no existe");
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        mVrPanoramaView.pauseRendering();
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mVrPanoramaView.resumeRendering();
+    }
+
+    @Override
+    protected void onDestroy() {
+        mVrPanoramaView.shutdown();
+        super.onDestroy();
     }
 }
 
